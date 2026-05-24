9eebdad69d62e75fdb920f3a3107d370d2764a43
varunnair1234 <varunnair304@gmail.com>
2026-05-24 10:04:08 -0700

    Update TissueSimulation.tsx

--- frontend/src/components/TissueSimulation.tsx
+++ frontend/src/components/TissueSimulation.tsx
@@ -140,7 +140,7 @@ export default function TissueSimulation() {
               layers={layers}
               style={{
                 position: "absolute",
-                top: 0, left: 0, width: "100%", height: "100%",
+                top: "0", left: "0", width: "100%", height: "100%",
                 background: "transparent",
               }}
             />
